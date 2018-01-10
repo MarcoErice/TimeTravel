a8d3348c52229c44db7a21c93d3ff1ea8bbf701f
MarcoErice <marco.erice@gmail.com>
2018-01-10 09:07:05 +0100

    Add project files.

---
+++ TimeTravel/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TimeTravel.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
